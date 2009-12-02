be2408bc5a54d4010e11012cdace8a04df29d7b2
Justin Lewis <jtl1728@rit.edu>
2009-12-02 18:53:53 -0500

    Added ability to add/remove files from file list.

--- FileShare.activity/FileShareActivity.py
+++ FileShare.activity/FileShareActivity.py
@@ -4,19 +4,41 @@ import sys, os
 
 import gtk
 
+from sugar.graphics.objectchooser import ObjectChooser
+
 class FileShareActivity(activity.Activity):
     def requestAddFile(self, widget, data=None):
         logging.info('Requesting to add file')
 
+	chooser = ObjectChooser()
+        try:
+	    if chooser.run() == gtk.RESPONSE_ACCEPT:
+	    	jobject = chooser.get_selected_object()
+		self._addFileToList(jobject)
+
+	finally:
+	    chooser.destroy()
+	    del chooser
+
     def requestRemFile(self, widget, data=None):
         logging.info('Requesting to delete file')
-	print "REQUEST DEL"
-	print self.treeview.get_selection().get_selected()
-        
+	if self.treeview.get_selection().count_selected_rows() != 0:
+	    model, iter = self.treeview.get_selection().get_selected()
+	    del self.sharedFiles[model.get_value(iter, 1)]
+	    model.remove( iter )
+
+
+    def _addFileToList(self, jObject):
+	self.sharedFiles[jObject.metadata['activity_id']] = jObject
+	modle = self.treeview.get_model()
+	modle.append(None, [jObject.metadata['title'],jObject.metadata['activity_id']])
+
     def __init__(self, handle):
         activity.Activity.__init__(self, handle)
 	
         print "activity running"
+
+	self.sharedFiles = {}
         self.set_title('File Share')
         
 	# Create Toolbox
@@ -39,37 +61,33 @@ class FileShareActivity(activity.Activity):
 	table = gtk.Table(rows=10, columns=1, homogeneous=False)
 
 
-	self.treestore = gtk.TreeStore(str)
-	
-	fileDisplayList = gtk.ListStore(str,int)
-
-	for name in range(4):
-	    self.treestore.append(None, ['Fake File %i' % name])
-
+	self.treestore = gtk.TreeStore(str,str)
 	self.treeview = gtk.TreeView(self.treestore)
 
 	# create the TreeViewColumn to display the data
-	self.tvcolumn = gtk.TreeViewColumn('Shared Files')
-	self.treeview.append_column(self.tvcolumn)
+	colName = gtk.TreeViewColumn('File Name')
+	colHash = gtk.TreeViewColumn('File Hash')
+	
+	self.treeview.append_column(colName)
+	self.treeview.append_column(colHash)
 
 	# create a CellRendererText to render the data
 	self.cell = gtk.CellRendererText()
 
 	# add the cell to the tvcolumn and allow it to expand
-	self.tvcolumn.pack_start(self.cell, True)
+	colName.pack_start(self.cell, True)
+	colHash.pack_start(self.cell, True)
 
-	# set the cell "text" attribute to column 0 - retrieve text
+	# set the cell "text" attribute- retrieve text
 	# from that column in treestore
-	self.tvcolumn.add_attribute(self.cell, 'text', 0)
+	colName.add_attribute(self.cell, 'text', 0)
+	colHash.add_attribute(self.cell, 'text', 1)
 	
 	# make it searchable
-   	#self.treeview.set_search_column(0)
+   	self.treeview.set_search_column(0)
 
 	# Allow sorting on the column
-	#self.tvcolumn.set_sort_column_id(0)
-	
-   	# Allow drag and drop reordering of rows
-   	#self.treeview.set_reorderable(True)
+	colName.set_sort_column_id(0)
 
 	table.attach(hbbox,0,1,0,1)
 	table.attach(self.treeview,0,1,1,10)
